d39e30f9490d645cb6762ea82ec83981838b5927
Talitha Ciancio <titalita35@hotmail.com>
2022-07-16 11:03:18 -0300

    API test

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard/indes";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App;
