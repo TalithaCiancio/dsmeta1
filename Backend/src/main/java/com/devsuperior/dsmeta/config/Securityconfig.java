2fc9616f8a847999765c5122f490ff2274f99548
Talitha Ciancio <titalita35@hotmail.com>
2022-07-17 13:45:20 -0300

    fixed

--- Backend/src/main/java/com/devsuperior/dsmeta/config/Securityconfig.java
+++ Backend/src/main/java/com/devsuperior/dsmeta/config/Securityconfig.java
@@ -14,6 +14,8 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	
 
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
